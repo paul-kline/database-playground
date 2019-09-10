9dd3ebd65598087f9bde7e20791f20dad9385231
Paul Kline <pauliankline@gmail.com>
2019-09-10 10:01:42 -0500

    first commit

---
+++ components/Editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Editor</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Editor extends Vue {
+  // Data property
+  myDataProperty: string
+
+  // Lifecycle hook
+  mounted() {
+    this.myDataProperty = 'Boop'
+  }
+
+  // Component method
+  updateMyProperty($event) {
+    this.myDataProperty = $event.target.value
+  }
+}
+</script>
---
+++ pages/editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Editor page</p>
+    <Editor />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import Editor from '@/components/Editor.vue'
+@Component({ components: { Editor } })
+export default class EditorPage extends Vue {
+  // Data property
+  myDataProperty: string
+
+  // Lifecycle hook
+  mounted() {
+    this.myDataProperty = 'Boop'
+  }
+}
+</script>
